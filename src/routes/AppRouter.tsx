32cfd2bd75a6f0cd55914255617e9a5da6b9688e
Andres Camacho <andrescamachoclaros@gmail.com>
2023-07-10 20:36:21 -0500

    Se instala tailwind y se empieza implementación del navbar

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,14 +1,18 @@
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { HomeScreen } from '../components/Home/HomeScreen';
 import { LoginScreen } from '../components/Login/LoginScreen';
 import { RegisterScreen } from '../components/Register/RegisterScreen';
+import { Navbar } from '../components/Ui/Navbar';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
+      <Navbar/>
         <Routes>
             <Route path='/login' element={<LoginScreen/>} />
             <Route path='/register' element={<RegisterScreen/>} />
+            <Route path='/' element={<HomeScreen/>} />
 
 
         </Routes>
